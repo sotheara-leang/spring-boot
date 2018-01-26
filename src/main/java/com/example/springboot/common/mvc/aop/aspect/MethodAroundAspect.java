90a3dcb4f741a593fd6176d9fd00183b68ea8fbc
sotheara.leang <sotheara.leang@MCNC-SOTHEARA.mobilecnc.com>
2018-01-26 13:33:53 +0700

    Commit

--- src/main/java/com/example/springboot/common/mvc/aop/aspect/MethodAroundAspect.java
+++ src/main/java/com/example/springboot/common/mvc/aop/aspect/MethodAroundAspect.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.aop.aspectj.MethodInvocationProceedingJoinPoint;
 
 import com.example.springboot.common.mvc.filter.BasicFilterChain;
 import com.example.springboot.common.mvc.filter.Filter;
-import com.example.springboot.common.mvc.filter.MethodInvocationFilter;
+import com.example.springboot.common.mvc.filter.FilterChain;
 import com.example.springboot.common.mvc.model.Message;
 
 public class MethodAroundAspect {
@@ -29,5 +30,19 @@ public class MethodAroundAspect {
 		filterChain.doFilter(new Message<Object>(jointPoint), response);
 		
 		return response.getBody();
+	}
+	
+	class MethodInvocationFilter implements Filter {
+
+		@Override
+		public void doFilter( Message<Object> request, Message<Object> response, FilterChain filterChain ) throws Throwable {
+			if (request == null || (request.getBody() != null 
+					&& MethodInvocationProceedingJoinPoint.class.isAssignableFrom( request.getBody().getClass() ))) {
+				MethodInvocationProceedingJoinPoint jp = (MethodInvocationProceedingJoinPoint) request.getBody();
+				response.setBody( jp.proceed() );
+			} else {
+				filterChain.doFilter( request, response );
+			}
+		}
 	}
 }
